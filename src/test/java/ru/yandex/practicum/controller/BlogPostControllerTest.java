6b440a16414ec345bf71189097920157a9a95330
Lerussia <appleasedj@inbox.ru>
2026-05-03 19:00:15 +0300

    Изменение проекта на spring boot + проверка работы

--- src/test/java/ru/yandex/practicum/controller/BlogPostControllerTest.java
+++ src/test/java/ru/yandex/practicum/controller/BlogPostControllerTest.java
@@ -3,27 +3,22 @@ package ru.yandex.practicum.controller;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
-import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
-import ru.yandex.practicum.WebConfiguration;
-import ru.yandex.practicum.configuration.DataSourceConfiguration;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@SpringJUnitConfig(classes = {
-        DataSourceConfiguration.class,
-        WebConfiguration.class,
-})
-@WebAppConfiguration
+@SpringBootTest
+@AutoConfigureMockMvc
 @TestPropertySource(locations = "classpath:test-application.properties")
 class BlogPostControllerTest {
 
